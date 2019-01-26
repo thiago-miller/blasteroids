a78b63eec9e756428d1ac87916b41d2db8561ea4
Thiago Miller <tmiller@mochsl.org.br>
2019-01-26 03:54:53 -0200

    Cleanup main.c

--- src/main.c
+++ src/main.c
@@ -1,91 +1,10 @@
-#include <allegro5/allegro5.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <blasteroids.h>
 
-bool done;
-ALLEGRO_EVENT_QUEUE* event_queue;
-ALLEGRO_TIMER* timer;
-ALLEGRO_DISPLAY* display;
-
-void abort_game(const char* message)
+int
+main (int argc, char *argv[])
 {
-	printf("%s \n", message);
-	exit(1);
-}
-
-void init(void)
-{
-	if (!al_init())
-		abort_game("Failed to initialize allegro");
-
-	if (!al_install_keyboard())
-		abort_game("Failed to install keyboard");
-
-	timer = al_create_timer(1.0 / 60);
-	if (!timer)
-		abort_game("Failed to create timer");
-
-	al_set_new_display_flags(ALLEGRO_WINDOWED);
-	display = al_create_display(640, 480);
-	if (!display)
-		abort_game("Failed to create display");
-
-	event_queue = al_create_event_queue();
-	if (!event_queue)
-		abort_game("Failed to create event queue");
-
-	al_register_event_source(event_queue, al_get_keyboard_event_source());
-	al_register_event_source(event_queue, al_get_timer_event_source(timer));
-	al_register_event_source(event_queue, al_get_display_event_source(display));
-
-	done = false;
-}
-
-void cleanup(void)
-{
-	if (timer)
-		al_destroy_timer(timer);
-
-	if (display)
-		al_destroy_display(display);
-
-	if (event_queue)
-		al_destroy_event_queue(event_queue);
-}
-
-void game_loop(void)
-{
-	bool redraw = true;
-	al_start_timer(timer);
-
-	while (!done) {
-		ALLEGRO_EVENT event;
-		al_wait_for_event(event_queue, &event);
-
-		if (event.type == ALLEGRO_EVENT_TIMER) {
-			redraw = true;
-			//update_logic();
-		}
-		else if (event.type == ALLEGRO_EVENT_KEY_DOWN) {
-			if (event.keyboard.keycode == ALLEGRO_KEY_ESCAPE) {
-				done = true;
-			}
-		//get_user_input();
-		}
-
-		if (redraw && al_is_event_queue_empty(event_queue)) {
-			redraw = false;
-			al_clear_to_color(al_map_rgb(0, 0, 0));
-			//update_graphics();
-			al_flip_display();
-		}
-	}
-}
-
-int main(int argc, char* argv[])
-{
-	init();
-	game_loop();
-	cleanup();
+	blasteroids_init ();
+	blasteroids_game_loop ();
+	blasteroids_shutdown ();
 	return 0;
 }
